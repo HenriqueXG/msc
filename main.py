a9e0d2466ef49974df8fb146a5d0606a93befbce
HenriqueXG <henrique.xg@gmail.com>
2018-11-10 00:29:35 -0200

    Test indoor in parallel and media

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+# Main - Workspace
 ## Henrique X. Goulart
 
 import json
 import sys
 import os
 import pickle
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 import multiprocessing as mp
@@ -12,8 +12,7 @@ from PIL import Image
 from pathlib import Path
 from scipy import spatial
 from sklearn.svm import SVC
-
-random.seed(42)
+from sklearn.preprocessing import MinMaxScaler
 
 config = {}
 with open('config.json', 'r') as fp:
@@ -28,14 +27,14 @@ except ImportError:
     from src.graph import Graph
     from src.declarative import Declarative
 
-def open_indoor_test():
+def open_indoor_test(declarative):
     # Open tess MIT Indoor 67 files
 
-    path_test = os.path.join(declarative.config['path'], 'data', 'TestImages.txt')
-    path_train = os.path.join(declarative.config['path'], 'data', 'TrainImages.txt')
+    path_test = os.path.join(config['path'], 'data', 'TestImages.txt')
+    path_train = os.path.join(config['path'], 'data', 'TrainImages.txt')
     length = len(open(path_test).readlines())
 
-    test_indoor_path = os.path.join(declarative.config['path'], 'data', 'test_indoor.pkl')
+    test_indoor_path = os.path.join(config['path'], 'data', 'test_indoor.pkl')
     test_data = None
     if os.path.exists(test_indoor_path): # Check if file exists
         with open(test_indoor_path, 'rb') as fp:
@@ -46,7 +45,7 @@ def open_indoor_test():
 
         with open(path_test, 'r', encoding='ISO-8859-1') as archive:
             for line in archive:
-                path = os.path.join(declarative.config['path'], 'data', 'MITImages', line.strip())
+                path = os.path.join(config['path'], 'data', 'MITImages', line.strip())
                 img = Image.open(path)
                 img = declarative.img_channels(img)
 
@@ -60,7 +59,7 @@ def open_indoor_test():
         with open(test_indoor_path, 'wb') as fp:
             pickle.dump(test_data, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
-    train_indoor_path = os.path.join(declarative.config['path'], 'data', 'train_indoor.pkl')
+    train_indoor_path = os.path.join(config['path'], 'data', 'train_indoor.pkl')
     train_data = None
     if os.path.exists(train_indoor_path): # Check if file exists
         with open(train_indoor_path, 'rb') as fp:
@@ -71,7 +70,7 @@ def open_indoor_test():
 
         with open(path_train, 'r', encoding='ISO-8859-1') as archive:
             for line in archive:
-                path = os.path.join(declarative.config['path'], 'data', 'MITImages', line.strip())
+                path = os.path.join(config['path'], 'data', 'MITImages', line.strip())
                 img = Image.open(path)
                 img = declarative.img_channels(img)
 
@@ -87,14 +86,20 @@ def open_indoor_test():
 
     return train_data, test_data, length
 
-def test_indoor_svm(train_data, test_data, output):
+def test_indoor_svm(svm, train_data, test_data, output):
     # SVM test on MIT Indoor 67
     print('Testing SVM...')
 
-    svm = SVC(kernel='rbf', probability=True).fit(train_data['X'],train_data['Y'])
+    pred = {}
+    pred['model'] = 'svm'
 
-    pred = svm.predict_proba(test_data['X'])
-    # acc = svm.score(test_data['X'], test_data['Y'])
+    for idx in range(len(test_data['X'])):
+        pred_ = svm.predict_proba([test_data['X'][idx]])[0]
+        pred[idx] = pred_
+    pred['classes'] = svm.classes_
+
+    acc = svm.score(test_data['X'], test_data['Y'])
+    pred['acc'] = acc
 
     output.put(pred)
 
@@ -107,7 +112,7 @@ def test_indoor_graph(declarative, graph, test_data, length, output):
 
     with open(path_test, 'r', encoding='ISO-8859-1') as archive:
         for idx, line in enumerate(archive):
-            sys.stdout.write('Testing graph... ' + str(idx+1) + '/' + str(length) + '\n')
+            sys.stdout.write('Testing graph... ' + str(idx+1) + '/' + str(length) + '\r')
             
             path = os.path.join(declarative.config['path'], 'data', 'MITImages', line.strip())
             img = Image.open(path)
@@ -167,12 +172,16 @@ def test_indoor_graph(declarative, graph, test_data, length, output):
 
 def test_indoor_cosine(declarative, test_data, length, output):
     # Cosine test on MIT Indoor 67
-    print('Testing cosine')
+    print('Testing cosine...')
 
     path_test = os.path.join(declarative.config['path'], 'data', 'TestImages.txt')
 
     corr = 0.0
-    pred = []
+    pred = {}
+    pred['model'] = 'cosine'
+
+    classes_ = [class_name for class_name, _ in declarative.declarative_data['scene_vectors'].items()]
+    pred['classes'] = classes_
 
     with open(path_test, 'r', encoding='ISO-8859-1') as archive:
         for idx, _ in enumerate(archive):
@@ -181,12 +190,13 @@ def test_indoor_cosine(declarative, test_data, length, output):
 
             distance = np.inf
             pred_class = ''
-            _pred = []
+
+            pred_ = []
 
             for class_name, class_vec in declarative.declarative_data['scene_vectors'].items():
-                d = 1.0 - spatial.distance.cosine(vec, class_vec)
+                d = spatial.distance.cosine(vec, class_vec)
 
-                _pred.append(d)
+                pred_.append(1.0 - d)
 
                 if d < distance:
                     pred_class = class_name
@@ -194,40 +204,98 @@ def test_indoor_cosine(declarative, test_data, length, output):
 
             if pred_class == scene_class:
                 corr += 1.0
-            
-            pred.append(_pred)
+
+            pred[idx] = pred_
+    
+    pred['acc'] = corr/length
     
     output.put(pred)
 
-if __name__ == '__main__':
-    output = mp.Queue()
+def test_indoor(svm, declarative, graph, train_data, test_data, alphas, output_test):
+    # Testing - Scenes Vectors - MIT Indoor 67
+    for alpha in alphas:
+        output = mp.Queue()
 
+        processes = []
+        processes.append(mp.Process(target=test_indoor_cosine, args=(declarative, test_data, length, output)))
+        processes.append(mp.Process(target=test_indoor_svm, args=(svm, train_data, test_data, output)))
+        # processes.append(mp.Process(target=test_indoor_graph, args=(declarative, graph, test_data, length, output)))
+
+        for p in processes:
+            p.start()
+
+        results = [output.get() for p in processes]
+
+        for p in processes:
+            p.join()
+
+        for r in results:
+            if r['model'] == 'svm':
+                svm_r = r
+            elif r['model'] == 'cosine':
+                cos_r = r
+        
+        pred = []
+
+        for idx in range(len(test_data['X'])):
+            v1 = svm_r[idx]
+            v2 = cos_r[idx]
+
+            # MinMax Normalization
+            v1 = [(x-min(v1))/(max(v1)-min(v1)) for x in v1]
+            v2 = [(x-min(v2))/(max(v2)-min(v2)) for x in v2]
+
+            pred_ = [v1[i]*alpha + v2[i]*(1.0 - alpha) for i in range(len(v1))]
+            pred.append(pred_)
+
+        corr = 0.0
+        for idx, pred_ in enumerate(pred):
+            max_i = 0
+            max_v = 0
+            for i in range(len(pred_)):
+                if pred_[i] > max_v:
+                    max_v = pred_[i]
+                    max_i = i
+            pred_class = svm_r['classes'][max_i]
+            scene_class = test_data['Y'][idx]
+
+            if pred_class == scene_class:
+                corr += 1.0
+
+        output_test.put((alpha, corr/length))
+
+if __name__ == '__main__':
     # graph = Graph(arch = config['arch_obj'])
     declarative = Declarative()
 
     print('Testing - Scenes Vectors - MIT Indoor 67')
-    train_data, test_data, length = open_indoor_test()
+    train_data, test_data, length = open_indoor_test(declarative)
+    
+    svm = SVC(kernel='rbf', probability=True).fit(train_data['X'], train_data['Y'])
+
+    output_test = mp.Queue()
+    NB_JOBS = mp.cpu_count()
+    xs = np.arange(0.0, 1.01, 0.01)
+    alpha_parts = np.array_split(xs, NB_JOBS)
 
     processes = []
-    processes.append(mp.Process(target=test_indoor_cosine, args=(declarative, test_data, length, output)))
-    processes.append(mp.Process(target=test_indoor_svm, args=(train_data, test_data, output)))
-    # processes.append(mp.Process(target=test_indoor_graph, args=(declarative, graph, test_data, length, output)))
+    for part in alpha_parts:
+        processes.append(mp.Process(target=test_indoor, args=(svm, declarative, None, train_data, test_data, part, output_test)))
 
     for p in processes:
         p.start()
+
+    acc = [output_test.get() for p in processes]
+
     for p in processes:
         p.join()
 
-    results = [output.get() for p in processes]
 
-    svm_r = results[0]
-    cos_r = results[1]
+    acc = sorted(acc, key=lambda t: t[0])
+    x, y = map(list,zip(*acc))
 
-    r = [np.array(svm_r[i])*0.9 + np.array(cos_r[i])*0.1 for i in range(len(svm_r))]
+    plt.plot(x, y)
+    plt.xlabel('alpha')
+    plt.ylabel('accuracy')
+    plt.show()
 
-    # corr = 0.0
-    # for idx, sample in enumerate(r):
-    #     i = sample.index(max(sample))
-    #     if test_data['Y'][idx] == 
-    
-    # print('Accuracy on MIT Indoor 67: {} / {}'.format(results[0], results[1]))
