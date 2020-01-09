b155bfd1299257f0a63098910dcfd48e95e9b413
Henrique Xavier Goulart <henrique.goulart@einstein.br>
2020-01-09 13:37:05 -0300

    Exp B media

--- media/exp_b.py
+++ media/exp_b.py
@@ -10,27 +10,29 @@ config = {}
 with open('config.json', 'r') as fp:
     config = json.load(fp)
 
-alphas = ['0.0']
+pam_thresholds = ['0.5', '0.7', '0.9']
+alphas = ['0.0', '0.5', '1.0']
 datasets = [config['dataset']]
 kernels = ['rbf', 'sigmoid', 'linear']
 hidden_units = [100, 500, 1000, 2000]
 activations = ['relu', 'logistic', 'tanh']
 optimizers = ['adam', 'sgd']
 
-for alpha in alphas:
-    for dataset in datasets:
-        for kernel in kernels:
-            for hu in hidden_units:
-                for activation in activations:
-                    for optimizer in optimizers:
-                        path_r = path_r = os.path.join(config['path'], 'media', f"exp_b_{alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer}.pkl")
-                        if os.path.exists(path_r):
-                            with open(path_r, 'rb') as fp:
-                                results = pickle.load(fp)
-                            print(f"------ Result of: {alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer} ------")
-                            mean = sum(results)/len(results)
-                            print(f"Mean: {mean}")
-                            print('#####################')
+for pam_threshold in pam_thresholds:
+    for alpha in alphas:
+        for dataset in datasets:
+            for kernel in kernels:
+                for hu in hidden_units:
+                    for activation in activations:
+                        for optimizer in optimizers:
+                            path_r = path_r = os.path.join(config['path'], 'media', f"exp_b_{pam_threshold}_{alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer}.pkl")
+                            if os.path.exists(path_r):
+                                with open(path_r, 'rb') as fp:
+                                    results = pickle.load(fp)
+                                print(f"------ Result of: {pam_threshold}_{alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer} ------")
+                                mean = sum(results)/len(results)
+                                print(f"Mean: {mean}")
+                                print('#####################')
 
 path_r = os.path.join(config['path'], 'media', f"exp_b_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
 with open(path_r, 'rb') as fp:
