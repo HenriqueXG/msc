15f75e263f53d721db9b1a848a8e0c7c29a146a1
Henrique Xavier Goulart <henrique.goulart@einstein.br>
2020-02-04 11:30:57 -0300

    Media updated

--- media/exp_b.py
+++ media/exp_b.py
@@ -30,14 +30,43 @@ for pam_threshold in pam_thresholds:
                                 with open(path_r, 'rb') as fp:
                                     results = pickle.load(fp)
                                 print(f"------ Result of: {pam_threshold}_{alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer} ------")
-                                mean = sum(results)/len(results)
+                                mean = np.mean(results)
+                                std = np.std(results)
+                                print(f"Std: {std}")
                                 print(f"Mean: {mean}")
                                 print('#####################')
+                                fig, ax = plt.subplots()
 
-path_r = os.path.join(config['path'], 'media', f"exp_b_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
-with open(path_r, 'rb') as fp:
-    results = pickle.load(fp)
+                                x = np.arange(len(results))
+                                ax.bar(x, results)
 
+                                ax.set_ylim(0.6,1.0)
+                                ax.set_xlabel('Runs')
+                                ax.set_ylabel('Accuracy')
+                                ax.set_xticks(x, (i+1 for i in range(len(results))))
+
+                                plt.draw()
+                                fig.savefig(os.path.join(config['path'], 'media', 'images', f"exp_b_{pam_threshold}_{alpha}_{dataset}_{kernel}_{hu}_{activation}_{optimizer}.png".replace('.','_')), dpi=300)
+
+if config['dataset'] == 'sun397':
+    results = []
+    for i in range(10):
+        path_r = os.path.join(config['path'], 'media', f"exp_b_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_{i+1}_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
+        if os.path.exists(path_r):
+            with open(path_r, 'rb') as fp:
+                r = pickle.load(fp)
+                results.append(r[0])
+else:
+    path_r = os.path.join(config['path'], 'media', f"B_exp_b_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
+    with open(path_r, 'rb') as fp:
+        results = pickle.load(fp)
+
+mean = np.mean(results)
+std = np.std(results)
+print(f"Std: {std}")
+print(f"Mean: {mean}")
+
+plt.clf()
 fig, ax = plt.subplots()
 
 x = np.arange(len(results))
@@ -49,4 +78,3 @@ ax.set_ylabel('Accuracy')
 ax.set_xticks(x, (i+1 for i in range(len(results))))
 
 plt.savefig(os.path.join(config['path'], 'media', 'images', 'exp_b.png'), dpi=300)
-plt.show()
--- media/test_result.py
+++ media/test_result.py
@@ -5,6 +5,8 @@ import numpy as np
 import os
 import pickle
 import json
+import pandas
+from scipy import signal
 from sklearn import metrics
 from sklearn.metrics import precision_recall_curve, roc_curve
 
@@ -12,7 +14,12 @@ config = {}
 with open('config.json', 'r') as fp:
     config = json.load(fp)
 
-path_r = os.path.join(config['path'], 'media', f"test_result_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_1_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
+if config['dataset'] == 'indoor':
+    n_classes = 67
+else:
+    n_classes = 397
+
+path_r = os.path.join(config['path'], 'media', f"test_result_{config['pam_threshold']}_{config['alpha']}_{config['dataset']}_{config['kernel']}_{config['hidden_units']}_{config['activation']}_{config['optimizer']}.pkl")
 with open(path_r, 'rb') as fp:
     classes_predicted = pickle.load(fp)
 
@@ -32,17 +39,41 @@ print(corr/len(test_data['Y']))
 print(metrics.confusion_matrix(test_data['Y'], classes_predicted['classes']))
 
 # Print the precision and recall, among other metrics
-print(metrics.classification_report(test_data['Y'], classes_predicted['classes'], digits=3))
+report = metrics.classification_report(test_data['Y'], classes_predicted['classes'], digits=3, output_dict=True)
+df = pandas.DataFrame(report).transpose()
+
+path_table = os.path.join(config['path'], 'media', f"pr_{config['dataset']}.tex")
+with open(path_table, 'w') as fp:
+    fp.write(df.to_latex())
 
 precision = dict()
 recall = dict()
-for i in range(67):
+for i in range(n_classes):
     precision[i], recall[i], _ = precision_recall_curve(np.array(classes_predicted['y_test'])[:, i], np.array(classes_predicted['predictions'])[:, i])
     plt.plot(recall[i], precision[i], lw=2, label='class {}'.format(i))
 
 plt.xlabel("Recall")
 plt.ylabel("Precision")
-plt.title("Precision vs. Recall curve")
+plt.title("Precision vs Recall curves")
+print(precision[0])
+plt.savefig(os.path.join(config['path'], 'media', 'images', f"pr_{config['dataset']}.png"), dpi=300)
+plt.show()
 
-plt.savefig(os.path.join(config['path'], 'media', 'images', 'pr.png'), dpi=300)
+precisions = [signal.resample(i, 100) for i in precision.values()]
+means = []
+std = []
+for i in range(len(precisions[0].tolist())):
+    p = []
+    for j in range(len(precisions)):
+        p.append(precisions[j][i])
+    means.append(np.mean(p))
+    std.append(np.std(p))
+
+plt.plot(means, label='Mean')
+plt.plot(std, label='Std. deviation')
+plt.legend()
+plt.xlabel("Sample of Recall")
+plt.ylabel("Value")
+
+plt.savefig(os.path.join(config['path'], 'media', 'images', f"pr_obs_{config['dataset']}.png"), dpi=300)
 plt.show()
